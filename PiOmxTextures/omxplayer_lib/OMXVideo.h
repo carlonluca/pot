4b8df02954d2863e1a3149691fb5c53078908c53
Luca Carlon <carlon.luca@gmail.com>
2013-02-23 15:57:02 +0100

    Added sources for modified omxplayer.

---
+++ PiOmxTextures/omxplayer_lib/OMXVideo.h
@@ -0,0 +1,127 @@
+#pragma once
+/*
+ *      Copyright (C) 2010 Team XBMC
+ *      http://www.xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+
+#if defined(HAVE_OMXLIB)
+
+#include <QObject>
+
+#include "OMXCore.h"
+#include "OMXStreamInfo.h"
+
+#include <IL/OMX_Video.h>
+
+#include "BitstreamConverter.h"
+
+#include "OMXClock.h"
+#include "OMXReader.h"
+
+#include "guilib/Geometry.h"
+
+class OMX_VideoSurfaceElement;
+class OMX_TextureProvider;
+
+
+#define VIDEO_BUFFERS 60
+
+#define CLASSNAME "COMXVideo"
+
+class DllAvUtil;
+class DllAvFormat;
+class COMXVideo : public QObject
+{
+    Q_OBJECT
+public:
+  COMXVideo(OMX_TextureProvider* provider);
+  ~COMXVideo();
+
+  // Required overrides
+  bool SendDecoderConfig();
+  bool Open(
+          COMXStreamInfo &hints,
+          OMXClock *clock,
+          uint& textureId,
+          float display_aspect = 0.0f,
+          bool deinterlace = false,
+          bool hdmi_clock_sync = false
+          );
+  void Close(void);
+  unsigned int GetFreeSpace();
+  unsigned int GetSize();
+  OMXPacket *GetText();
+  int  DecodeText(uint8_t *pData, int iSize, double dts, double pts);
+  int  Decode(uint8_t *pData, int iSize, double dts, double pts);
+  void Reset(void);
+  void SetDropState(bool bDrop);
+  bool Pause();
+  bool Resume();
+  std::string GetDecoderName() { return m_video_codec_name; }
+  void SetVideoRect(const CRect& SrcRect, const CRect& DestRect);
+  int GetInputBufferSize();
+  void WaitCompletion();
+
+signals:
+  void textureReady(const uint& textureId);
+
+protected:
+  // Video format
+  bool              m_drop_state;
+  unsigned int      m_decoded_width;
+  unsigned int      m_decoded_height;
+
+  OMX_VIDEO_CODINGTYPE m_codingType;
+
+  COMXCoreComponent m_omx_text;
+  COMXCoreComponent m_omx_decoder;
+  COMXCoreComponent m_omx_render;
+  COMXCoreComponent m_omx_sched;
+  COMXCoreComponent m_omx_image_fx;
+  COMXCoreComponent *m_omx_clock;
+  OMXClock           *m_av_clock;
+
+  COMXCoreTunel     m_omx_tunnel_text;
+  COMXCoreTunel     m_omx_tunnel_decoder;
+  COMXCoreTunel     m_omx_tunnel_clock;
+  COMXCoreTunel     m_omx_tunnel_sched;
+  COMXCoreTunel     m_omx_tunnel_image_fx;
+  bool              m_is_open;
+
+  bool              m_Pause;
+  bool              m_setStartTime;
+  bool              m_setStartTimeText;
+
+  uint8_t           *m_extradata;
+  int               m_extrasize;
+
+  CBitstreamConverter   *m_converter;
+  bool              m_video_convert;
+  std::string       m_video_codec_name;
+
+  bool              m_deinterlace;
+  bool              m_hdmi_clock_sync;
+  bool              m_first_frame;
+  bool              m_first_text;
+
+  OMX_TextureProvider* m_provider;
+  OMX_BUFFERHEADERTYPE* m_eglBuffer;
+};
+
+#endif
